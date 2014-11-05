b9326a98b84ada23ac97a03f286c0aac852c25b8
JamesPF <jamesfelz@gmail.com>
2014-11-04 19:16:31 -0800

    full rails crud functionality for both shows and posts

--- artist-website/config/routes.rb
+++ artist-website/config/routes.rb
@@ -3,13 +3,13 @@ Rails.application.routes.draw do
   root 'home#index'
 
   # These routes are for shows
-  get 'tourdate/index' => 'tourdate#index', as: :tourdate
+  get 'tourdate/' => 'tourdate#index', as: :tourdate
 
   get 'tourdate/new' => 'tourdate#new', as: :new
 
   get 'tourdate/:id' => 'tourdate#show', as: :single_tourdate
 
-  post 'tourdate/index' => 'tourdate#create', as: :new_tourdate
+  post 'tourdate/' => 'tourdate#create', as: :new_tourdate
 
   get 'tourdate/:id/edit' =>  'tourdate#edit', as: :edit_tourdate
 
@@ -19,13 +19,13 @@ Rails.application.routes.draw do
 
 
   # These routes are for news posts
-  get 'post/index' => 'post#index', as: :post
+  get 'post/' => 'post#index', as: :post
 
   get 'post/new' => 'post#new', as: :addpost
 
   get 'post/:id' => 'post#show', as: :single_post
 
-  post 'post/index' => 'post#create', as: :new_post
+  post 'post/' => 'post#create', as: :new_post
 
   get 'post/:id/edit' =>  'post#edit', as: :edit_post
 
